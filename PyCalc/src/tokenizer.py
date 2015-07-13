80c8568cdf7fb7ebaedd0e334744d41cbf7b74a5
Artem Mavrin <amavrin@ucsd.edu>
2015-07-13 01:05:03 -0700

    Class for tokenizing expressions

---
+++ PyCalc/src/tokenizer.py
@@ -0,0 +1,28 @@
+from src.peeker import Peeker
+
+reserved = ['=', '+', '-', '*', '/', '^', '(', ')', '|']
+
+def generate_tokens(line, start=0):
+    if start < len(line):
+        token = line[start]
+        if token == ' ':
+            yield from generate_tokens(line, start + 1)
+        elif token in reserved:
+            yield token, start, start + 1
+            yield from generate_tokens(line, start + 1)
+        else:
+            end = start + 1
+            while end < len(line):
+                if line[end] in reserved or line[end] == ' ':
+                    break
+                end += 1
+            yield line[start:end], start, end
+            yield from generate_tokens(line, end)
+
+def underline_token(line, start, end):
+    print(line)
+    print(start * ' ' + (end - start) * '^')
+
+class Tokenizer(Peeker):
+    def __init__(self, line):
+        super().__init__(generate_tokens(line))
