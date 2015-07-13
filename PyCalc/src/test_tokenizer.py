74cd8e014a67d5ba77bfa115573984cc1356c743
Artem Mavrin <amavrin@ucsd.edu>
2015-07-13 01:15:33 -0700

    test_tokenizer now uses the generate_tokens method instead of the Tokenizer class

--- PyCalc/src/test_tokenizer.py
+++ PyCalc/src/test_tokenizer.py
@@ -1,10 +1,9 @@
 from src.test import ConsoleTest
-from src.tokenizer import Tokenizer, underline_token
+from src.tokenizer import generate_tokens, underline_token
 
 @ConsoleTest
 def test_tokenizer(line):
-    tokenizer = Tokenizer(line)
-    for _, start, end in tokenizer:
+    for _, start, end in generate_tokens(line):
         underline_token(line, start, end)
 
 if __name__ == '__main__':
