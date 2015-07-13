6c4101bf90da11c651d2a02f4bd0c494a2d691ad
Artem Mavrin <amavrin@ucsd.edu>
2015-07-13 03:15:17 -0700

    Added functions for checking if a token is a function, a variable, an integer, or a float

--- PyCalc/src/parser.py
+++ PyCalc/src/parser.py
@@ -29,6 +29,33 @@ float_number ::= <float>
 '''
 from src.tokenizer import Tokenizer
 from src.tree import BinaryOperation, UnaryFunction, Value
+import re
+
+functions = ['exp', 'log', 'cos', 'sin', 'tan']
+
+def is_function(token):
+    return token in functions
+
+variable_regex = re.compile(r'^[a-zA-Z]\w*$')
+
+def is_variable(token):
+    return bool(variable_regex.match(token))
+
+def is_int(token):
+    try:
+        int(token)
+        return True
+    except ValueError:
+        return False
+
+def is_float(token):
+    try:
+        float(token)
+        return True
+    except ValueError:
+        return False
+
+
 
 class Parser(object):
     def parse(self, line):
